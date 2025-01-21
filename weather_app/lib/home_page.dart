33ea0f738f1e9ca450df7f47d3d4e99d63277793
Charitha Rajapakse <charitha.r@incentivio.com>
2025-01-21 13:20:20 +0530

    Adding home page provider changes

--- weather_app/lib/home_page.dart
+++ weather_app/lib/home_page.dart
@@ -1,103 +1,10 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 import 'package:weather_app/city_forecast.dart';
+import 'package:weather_app/provider/weather_provider.dart';
 
-import 'keys.dart';
-
-var _controller = TextEditingController();
-String searchLocation = '';
-
-String forecastLocation = '';
-double forecastValue = 0.0;
-String forecastIcon = '';
-
-bool isLoading = false;
-
-Set<String> searchListLocations = {};
-List<double> searchListValues = [];
-List<String> searchListIcons = [];
-
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  void initState() {
-    super.initState();
-
-    //Network call to get weather data
-    if (searchLocation.isNotEmpty) {
-      getCurrentWeather();
-    }
-  }
-
-  Future getCurrentWeather() async {
-    try {
-      setState(() {
-        isLoading = true;
-      });
-
-      final res = await http.get(
-        Uri.parse(
-            'https://api.weatherapi.com/v1/current.json?key=$apiKey&q=$searchLocation&aqi=no'),
-      );
-      //Getting data from network call
-      final data = jsonDecode(res.body);
-
-      if (res.statusCode == 200) {
-        setState(() {
-          //set data
-          forecastValue = (data['current']['temp_c']);
-          forecastLocation = (data['location']['name']);
-          forecastIcon = (data['current']['condition']['icon']);
-
-          //Adding to the location set
-          searchListLocations.add(forecastLocation);
-          searchListValues.add(forecastValue);
-          searchListIcons.add(forecastIcon);
-
-          isLoading = false;
-        });
-      } else {
-        setState(() {
-          isLoading = false;
-          _showDialog(context, (data['error']['message']));
-        });
-      }
-    } catch (e) {
-      setState(() {
-        isLoading = false;
-      });
-      throw e.toString();
-    }
-  }
-
-  //Error alertview
-  Future _showDialog(BuildContext context, String errorMessage) {
-    return showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Alert'),
-          content: Text(errorMessage),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Ok'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -112,53 +19,52 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          //Search Bar
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              textInputAction: TextInputAction.search,
-              controller: _controller,
-              decoration: InputDecoration(
-                hintText: 'Search',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.search),
-              ),
-              onSubmitted: (String value) {
-                setState(() {
-                  searchLocation = _controller.text;
-                  getCurrentWeather();
-                });
-              },
-              //New
-              onChanged: (value) {},
-            ),
-          ),
-
-          const SizedBox(
-            height: 10,
-          ),
-          //Search list
-          isLoading
-              ? CircularProgressIndicator()
-              : Expanded(
-                  child: ListView(
-                    scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    padding: EdgeInsets.zero,
-                    children: [
-                      for (var i = 0; i < searchListLocations.length; i++)
-                        CityForecast(
-                          value: searchListValues.elementAt(i),
-                          location: searchListLocations.elementAt(i),
-                          weatherIcon: searchListIcons.elementAt(i),
-                          indexItem: i.toString(),
-                        ),
-                    ],
+      body: Consumer<WeatherProvider>(
+        builder: (context, weatherProvider, child) {
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  textInputAction: TextInputAction.search,
+                  controller: weatherProvider.controller,
+                  decoration: const InputDecoration(
+                    hintText: 'Search',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.search),
                   ),
+                  onSubmitted: (String value) {
+                    weatherProvider.searchLocation = value;
+                    weatherProvider.getCurrentWeather(context);
+                  },
                 ),
-        ],
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+              //Search list
+              weatherProvider.isLoading
+                  ? const CircularProgressIndicator()
+                  : Expanded(
+                      child: ListView.builder(
+                        itemCount: weatherProvider.searchListLocations.length,
+                        itemBuilder: (context, index) {
+                          return CityForecast(
+                            value: weatherProvider.searchListValues
+                                .elementAt(index),
+                            location: weatherProvider.searchListLocations
+                                .elementAt(index),
+                            weatherIcon: weatherProvider.searchListIcons
+                                .elementAt(index),
+                            indexItem: index.toString(),
+                          );
+                        },
+                      ),
+                    ),
+            ],
+          );
+        },
       ),
     );
   }
